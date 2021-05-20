acf7651e5b60ddbef955c2c8ff6de15cccde2111
NotDevansh <75929370+NotDevansh@users.noreply.github.com>
2021-05-20 12:47:21 +0530

    Add files via upload

---
+++ SlingShot.js
@@ -0,0 +1,29 @@
+class SlingShot{
+    constructor(bodyA, myPoint){
+        var options = {
+            bodyA: bodyA,
+            pointB: myPoint,
+            stiffness: 0.04,
+            length: 10
+        }
+        this.pointB = myPoint
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+
+    Fly()
+    {
+      this.sling.bodyA = null;
+    }
+    
+    display(){
+        if(this.sling.bodyA)
+        {
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointB
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+        }
+    }
+    
+}
